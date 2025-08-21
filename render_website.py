fa6e03ae8ee7341d43fe2c5ec9ce837b6f546722
Al-Evst <al.evst96@gmail.com>
2025-08-21 20:22:26 +0200

    Add links to books txt files. + 2 colums

--- render_website.py
+++ render_website.py
@@ -1,15 +1,22 @@
 import json
 from jinja2 import Environment, FileSystemLoader
+from more_itertools import chunked  # позволяет делить список на чанки
 
-# Загружаем метаданные
+# Загружаем книги
 with open('static/meta_data.json', 'r', encoding='utf-8') as f:
     books = json.load(f)
 
+# Разбиваем книги на чанки по 2
+books_chunks = list(chunked(books, 2))
+
+# Настраиваем Jinja2
 env = Environment(loader=FileSystemLoader('templates'))
 template = env.get_template('index.html')
 
-output = template.render(books=books)
+# Рендерим
+output = template.render(books_chunks=books_chunks)
 
+# Сохраняем
 with open('index.html', 'w', encoding='utf-8') as f:
     f.write(output)
 
