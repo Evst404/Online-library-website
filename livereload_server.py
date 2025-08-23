87a5661c31f6b70c42dcee2d1a753924e47e5ca3
Al-Evst <al.evst96@gmail.com>
2025-08-23 22:11:22 +0200

    Add paginator

--- livereload_server.py
+++ livereload_server.py
@@ -1,28 +1,54 @@
 import json
 import os
+import math
 from jinja2 import Environment, FileSystemLoader
+from more_itertools import chunked
 from livereload import Server
 
-TEMPLATE_FILE = 'template.html'
-OUTPUT_FILE = 'index.html'
+TEMPLATE_FILE = 'index.html'  # Шаблон с пагинацией
+BOOKS_PER_PAGE = 10
+BOOKS_PER_ROW = 2
+PAGES_DIR = 'pages'
 
-def on_reload():
-    """Рендерим шаблон template.html в index.html"""
+os.makedirs(PAGES_DIR, exist_ok=True)
+
+def render_pages():
+    """Генерируем страницы с книгами по страницам"""
     # Загружаем книги
     with open('static/meta_data.json', 'r', encoding='utf-8') as f:
         books = json.load(f)
-    
+
     env = Environment(loader=FileSystemLoader('templates'))
     template = env.get_template(TEMPLATE_FILE)
-    rendered = template.render(books=books)
-    
-    with open(OUTPUT_FILE, 'w', encoding='utf-8') as f:
-        f.write(rendered)
-    
-    print(f"[INFO] Сгенерирован {OUTPUT_FILE} с {len(books)} книгами")
 
-# Запуск livereload сервера
-server = Server()
-server.watch('templates/*.html', on_reload)      
-server.watch('static/meta_data.json', on_reload) 
-server.serve(root='.', port=5500, host='127.0.0.1')
+    total_pages = math.ceil(len(books) / BOOKS_PER_PAGE)
+    pages_books = list(chunked(books, BOOKS_PER_PAGE))
+
+    for i, page_books in enumerate(pages_books, start=1):
+        books_chunks = list(chunked(page_books, BOOKS_PER_ROW))
+
+        rendered = template.render(
+            books_chunks=books_chunks,
+            page_number=i,
+            total_pages=total_pages
+        )
+
+        # Первая страница в корне
+        if i == 1:
+            output_file = 'index.html'
+        else:
+            output_file = os.path.join(PAGES_DIR, f'index{i}.html')
+
+        with open(output_file, 'w', encoding='utf-8') as f:
+            f.write(rendered)
+
+    print(f"[INFO] Сгенерировано {total_pages} страниц, index.html в корне, остальные в {PAGES_DIR}/")
+
+if __name__ == '__main__':
+    render_pages()  # Генерируем страницы сразу при старте
+
+    # Запуск livereload сервера
+    server = Server()
+    server.watch('templates/*.html', render_pages)
+    server.watch('static/meta_data.json', render_pages)
+    server.serve(root='.', port=5500, host='127.0.0.1')
--- render_website.py
+++ render_website.py
@@ -1,23 +1,47 @@
 import json
+import os
+import math
 from jinja2 import Environment, FileSystemLoader
-from more_itertools import chunked  # позволяет делить список на чанки
+from more_itertools import chunked
 
-# Загружаем книги
-with open('static/meta_data.json', 'r', encoding='utf-8') as f:
-    books = json.load(f)
+TEMPLATE_FILE = 'index.html'  # Шаблон с пагинацией
+PAGES_DIR = 'pages'
+BOOKS_PER_PAGE = 10
+BOOKS_PER_ROW = 2
 
-# Разбиваем книги на чанки по 2
-books_chunks = list(chunked(books, 2))
+os.makedirs(PAGES_DIR, exist_ok=True)
 
-# Настраиваем Jinja2
-env = Environment(loader=FileSystemLoader('templates'))
-template = env.get_template('index.html')
+def render_pages():
+    """Генерируем страницы с книгами по страницам"""
+    # Загружаем книги
+    with open('static/meta_data.json', 'r', encoding='utf-8') as f:
+        books = json.load(f)
 
-# Рендерим
-output = template.render(books_chunks=books_chunks)
+    env = Environment(loader=FileSystemLoader('templates'))
+    template = env.get_template(TEMPLATE_FILE)
 
-# Сохраняем
-with open('index.html', 'w', encoding='utf-8') as f:
-    f.write(output)
+    total_pages = math.ceil(len(books) / BOOKS_PER_PAGE)
+    pages_books = list(chunked(books, BOOKS_PER_PAGE))
 
-print("Сайт успешно сгенерирован!")
+    for i, page_books in enumerate(pages_books, start=1):
+        books_chunks = list(chunked(page_books, BOOKS_PER_ROW))
+
+        rendered = template.render(
+            books_chunks=books_chunks,
+            page_number=i,
+            total_pages=total_pages
+        )
+
+        # Первая страница в корне
+        if i == 1:
+            output_file = 'index.html'
+        else:
+            output_file = os.path.join(PAGES_DIR, f'index{i}.html')
+
+        with open(output_file, 'w', encoding='utf-8') as f:
+            f.write(rendered)
+
+    print(f"[INFO] Сгенерировано {total_pages} страниц, index.html в корне, остальные в {PAGES_DIR}/")
+
+if __name__ == '__main__':
+    render_pages()
